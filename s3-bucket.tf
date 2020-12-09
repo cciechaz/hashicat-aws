f6fd531992c0b490145997cad712fdd060e74499
cciechaz <charlie@spanningtree.co.uk>
2020-12-09 01:18:30 +0000

    added tags

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,6 +2,8 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket_prefix = "charlesgreenaway"
   acl    = "private"
+  tags = ["Department","Billable"]
+
 
   versioning = {
     enabled = true
