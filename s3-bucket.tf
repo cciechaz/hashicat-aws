9b508aef0886ed265706c6294e9969155b8956d2
cciechaz <charlie@spanningtree.co.uk>
2020-12-09 01:20:57 +0000

    corrected tags

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket_prefix = "charlesgreenaway"
   acl    = "private"
-  tags = ["Department","Billable"]
+  tags = map("Department","tech","Billable","yes")
 
 
   versioning = {
